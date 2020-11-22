f57e921d4c6c6c82d0812f6da810746ba147fa97
Ritik Kumar <ritik.kumar006@gmail.com>
2020-11-22 22:23:13 +0530

    Added new methods

--- Arrays/kth-smallest-element.cpp
+++ Arrays/kth-smallest-element.cpp
@@ -82,6 +82,131 @@ int solveByMinHeap(int arr[], int n, int k) {
     cout << newMinHeap.extractMin() << "\n";
 }
 
+// Using Max Heaps
+// Steps: 1. Build a max heap of the first k elements of the array. TC - O(k)
+// 2. For the rest of the elements, compare them with the root of the heap.
+// 3. If it is smaller than root, then make it root and call heapify for MH.
+// 4. ELse, ignore it
+// 5. After these steps, the root is the required element
+
+class MaxHeap {
+    int* harr;
+    int capacity;
+    int heapSize;
+    public:
+    MaxHeap(int arr[], int size) {
+        harr = arr;
+        heapSize = size;
+        int i = (heapSize - 1) / 2;
+        while(i >= 0) {
+            maxHeapify(i);
+            i++;
+        }
+    }
+    void maxHeapify(int i);
+    int parentNode(int i) { return (i - 1) / 2; }
+    int leftNode(int i) { return 2 * i + 1; }
+    int rightNode(int i) { return 2 * i + 2; }
+    int extractMax();
+    int getMax() { return harr[0]; }
+    void replaceMax(int x) {
+        harr[0] = x;
+        maxHeapify(0);
+    }
+
+};
+
+// To heapify a max heap rooted at index i, assuming that subtress are already heapified.
+void MaxHeap::maxHeapify(int i) {
+    int left = leftNode(i);
+    int right = rightNode(i);
+    int largest = i;
+    if(left < heapSize and harr[left] > harr[largest]) largest = left;
+    if(right < heapSize and harr[right] > harr[largest]) largest = right;
+    if(largest != i) {
+        swap(harr[largest], harr[i]);
+        maxHeapify(largest);
+    }
+}
+
+int solveByMaxHeap(int arr[], int n, int k) {
+    // Build a heap for the first k elements
+    MaxHeap newHeap(arr, k);
+
+    // For the rest of the elements, compare with the root of the heap.
+    for(int i = k; i < n; ++i) {
+        if(arr[i] < newHeap.getMax())
+            newHeap.replaceMax(arr[i]);
+    }
+
+    cout << newHeap.getMax() << "\n";
+
+}
+
+// Quick Select method
+// Optimization over the sorting method if quick sort is used as the sorting algorithm
+// Not to complete quick sort, but stop when the pivot element is the kth smallest element
+// Not recurr for both sides, but recur depeneding on the position of the pivot.
+// TC - O(n^2) in worst case but O(n) in average
+int solveByQuickSelect(int arr[], int l, int r, int k) {
+    if(k > 0 and k <= r - l + 1) {
+        // Partition the array around the pivot by choosing the last element as the pivot
+        int pos = partition(arr, l, r);
+        if(pos - l == k - 1) {
+            return arr[pos];
+        }
+        else if(pos - l > k - 1) {
+            return solveByQuickSelect(arr, l, pos - 1, k);
+        }
+        else {
+            return solveByQuickSelect(arr, pos + 1, r, k - pos + l - 1);
+        }
+    }
+
+    return INT_MAX;
+}
+
+int partition(int arr[], int l, int r) {
+    int x = arr[r], i = l - 1;
+    for(int j = l; j <= r - 1; j++) {
+        if(arr[j] <= x) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[r]);
+    return i;
+}
+
+// Slightly optimized version of quick select, by choosing a pivot randomly.
+// Steps: 1. Generate a random index between l and r, swap it with the last element
+// 2. Implement standard partition
+int betterQuickSelect(int arr[], int l, int r, int k) {
+    if(k > 0 and k <= r - l + 1) {
+        int pos = randomPartition(arr, l, r);
+
+        if(pos - l == k - 1) {
+            return arr[pos];
+        }
+        else if(pos - l > k - 1) {
+            // Recur for left subarray
+            return betterQuickSelect(arr, l, pos - 1, k);
+        }
+        else {
+            // Recurr for the right subarray
+            return betterQuickSelect(arr, pos + 1, r, k - pos + l - 1);
+        }
+    }
+    return INT_MAX;
+}
+
+int randomPartition(int arr[], int l, int r) {
+    int n = r - l + 1;
+    int pivot = rand() % n;
+    swap(arr[l + pivot], arr[r]);
+    return partition(arr, l , r);
+}
+
 void solve() {
     int n, k;
     cin >> n >> k;
@@ -89,7 +214,9 @@ void solve() {
     for(int i = 0; i < n; ++i) cin >> arr[i];
     solveBySort(arr, n, k);
     solveByMinHeap(arr, n, k);
-
+    solveByMaxHeap(arr, n, k);
+    solveByQuickSelect(arr, 0, n - 1, k);
+    betterQuickSelect(arr, 0, n - 1, k);
 }
 
 int main() {
--- Arrays/sorting.cpp
+++ Arrays/sorting.cpp
@@ -100,7 +100,7 @@ void quickSort(int arr[], int low, int high) {
 int partition(int arr[], int low, int high) {
     int pivot = arr[high];
     int i = low - 1;
-    for(int j = low; j <= high; ++j) {
+    for(int j = low; j < high; ++j) {
         if(arr[j] < pivot) {
             i++;
             swap(arr[i], arr[j]);
